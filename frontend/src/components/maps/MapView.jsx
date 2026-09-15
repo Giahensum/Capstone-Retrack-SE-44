6d50bc4b98ab3845b00256a30078994efb6f2444
Giahensum <gianam2k4@gmail.com>
2026-09-15 20:21:13 +0700

    refactor: switch from Mapbox to Goong.io (Vietnam map provider)

--- frontend/src/components/maps/MapView.jsx
+++ frontend/src/components/maps/MapView.jsx
@@ -1,4 +1,4 @@
-﻿// TODO: Implement MapView
+﻿// TODO: Implement MapView using Goong.io (Mapbox GL JS compatible)
 export default function MapView() {
-  return <div>MapView</div>;
+  return <div>MapView - Goong.io</div>;
 }
